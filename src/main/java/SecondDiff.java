bfb80df1cc4a86285763da15f151880b1dd3ee3f
gemsoundart <baefrance@gmail.com>
2021-06-01 14:16:31 +0200

    Stay same

--- src/main/java/SecondDiff.java
+++ src/main/java/SecondDiff.java
@@ -2,6 +2,6 @@ public class SecondDiff {
     public void secDiff(){
         System.out.println("secDiff-1");
 
-        System.out.println("new branch create");
+        System.out.println("stay same");
     }
 }
