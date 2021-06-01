fe0d4e95712b80f106b32b5f257290f98dbb3cb5
gemsoundart <baefrance@gmail.com>
2021-06-01 13:22:08 +0200

    First diff new line Second diff new line

--- src/main/java/Diff.java
+++ src/main/java/Diff.java
@@ -4,5 +4,9 @@ public class Diff {
         System.out.println("diff-2");
         System.out.println("diff-3");
         System.out.println("diff-4");
+
+        System.out.println("diff-1 with secDiif-1");
+
+
     }
 }
--- src/main/java/SecondDiff.java
+++ src/main/java/SecondDiff.java
@@ -1,2 +1,5 @@
 public class SecondDiff {
+    public void secDiff(){
+        System.out.println("secDiff-1");
+    }
 }
